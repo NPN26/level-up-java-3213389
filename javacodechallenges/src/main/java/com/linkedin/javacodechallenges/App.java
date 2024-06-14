90310f23ee022af945de5458cc42db7e6866f9cc
NPN26 <133509488+NPN26@users.noreply.github.com>
2024-06-14 13:36:09 +0400

    implemented functionality

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        return password.length() > 5 && password.matches(".*[A-Z].*") && password.matches(".*[a-z].*") && password.matches(".*\\d.*");
     }
 
     public static void main(String[] args) {
