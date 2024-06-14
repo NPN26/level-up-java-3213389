a13a33043ebbf3280ea13be2cf1bcb7a2038fb89
NPN26 <133509488+NPN26@users.noreply.github.com>
2024-06-14 22:01:17 +0400

    implemented functionality

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,44 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person(String firstName, String lastName, int age) {
+      this.setFirstName(firstName);
+      this.setLastName(lastName);
+      this.setAge(age);
+  }
+
+  public void introduction() {
+      System.out.println("Hi, i am " + getFirstName() + " " + getLastName() + ",and i am " + getAge() + " years old.");
+  }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+      if (age < 0)
+          System.out.println("Age must be a positive integer.");
+      else
+        this.age = age;
+    }
 }
